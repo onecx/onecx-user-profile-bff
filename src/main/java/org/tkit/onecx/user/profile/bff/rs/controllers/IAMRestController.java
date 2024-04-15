f60f3793706b3c131d3530e0466d7ecfbd79cd43
Jannik Steenken <jannik.steenken@capgemini.com>
2024-04-15 16:25:11 +0200

    feat: controller annotations

--- src/main/java/org/tkit/onecx/user/profile/bff/rs/controllers/IAMRestController.java
+++ src/main/java/org/tkit/onecx/user/profile/bff/rs/controllers/IAMRestController.java
@@ -1,15 +1,21 @@
 package org.tkit.onecx.user.profile.bff.rs.controllers;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.core.Response;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.tkit.onecx.user.profile.bff.rs.mappers.IAMMapper;
+import org.tkit.quarkus.log.cdi.LogService;
 
 import gen.org.tkit.onecx.iam.kc.client.api.AdminUserControllerApi;
 import gen.org.tkit.onecx.user.profile.bff.rs.internal.IamApiService;
 import gen.org.tkit.onecx.user.profile.bff.rs.internal.model.UserResetPasswordRequestDTO;
 
+@ApplicationScoped
+@Transactional(value = Transactional.TxType.NOT_SUPPORTED)
+@LogService
 public class IAMRestController implements IamApiService {
 
     @Inject
