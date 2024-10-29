a5630396bbaae0870359800d991f18adc6b78bf5
Jorden_Reuter <jorden.reuter@capgemini.com>
2024-10-29 08:19:22 +0100

    feat: added avatar admin controller

---
+++ src/main/java/org/tkit/onecx/user/profile/bff/rs/controllers/UserAvatarAdminRestController.java
@@ -0,0 +1,80 @@
+package org.tkit.onecx.user.profile.bff.rs.controllers;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.Response;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
+import org.tkit.onecx.user.profile.bff.rs.mappers.ExceptionMapper;
+import org.tkit.onecx.user.profile.bff.rs.mappers.UserProfileMapper;
+import org.tkit.quarkus.log.cdi.LogService;
+
+import gen.org.tkit.onecx.image.bff.clients.api.AvatarInternalApi;
+import gen.org.tkit.onecx.image.bff.clients.model.ImageInfo;
+import gen.org.tkit.onecx.user.profile.bff.rs.internal.UserAvatarAdminApiService;
+import gen.org.tkit.onecx.user.profile.bff.rs.internal.model.RefTypeDTO;
+
+@ApplicationScoped
+@Transactional(value = Transactional.TxType.NOT_SUPPORTED)
+@LogService
+public class UserAvatarAdminRestController implements UserAvatarAdminApiService {
+    @Inject
+    @RestClient
+    AvatarInternalApi client;
+
+    @Inject
+    UserProfileMapper mapper;
+
+    @Inject
+    ExceptionMapper exceptionMapper;
+
+    @Inject
+    HttpHeaders headers;
+
+    @ServerExceptionMapper
+    public Response restException(WebApplicationException ex) {
+        return exceptionMapper.restException(ex);
+    }
+
+    @Override
+    public Response deleteUserAvatarById(String id) {
+        try (Response response = client.deleteImage(id)) {
+            return Response.status(response.getStatus()).build();
+        }
+    }
+
+    @Override
+    public Response getUserAvatarById(String id, RefTypeDTO refType) {
+        Response.ResponseBuilder responseBuilder = null;
+        try (Response response = client.getImage(id, mapper.map(refType))) {
+            var contentType = response.getHeaderString(HttpHeaders.CONTENT_TYPE);
+            var contentLength = response.getHeaderString(HttpHeaders.CONTENT_LENGTH);
+            var body = response.readEntity(byte[].class);
+
+            if (contentType != null && body.length != 0) {
+                responseBuilder = Response.status(response.getStatus())
+                        .header(HttpHeaders.CONTENT_TYPE, contentType)
+                        .header(HttpHeaders.CONTENT_LENGTH, contentLength)
+                        .entity(body);
+            } else {
+                responseBuilder = Response.status(Response.Status.BAD_REQUEST);
+            }
+
+            return responseBuilder.build();
+        }
+
+    }
+
+    @Override
+    public Response uploadAvatarById(String id, RefTypeDTO refType, byte[] body) {
+        try (Response response = client.uploadImage(headers.getLength(), id, mapper.map(refType), body)) {
+            var imageInfo = response.readEntity(ImageInfo.class);
+            return Response.status(response.getStatus())
+                    .entity(mapper.map(imageInfo)).build();
+        }
+    }
+}
